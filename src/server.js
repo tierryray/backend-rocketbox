2929c0604f7148cc515a8e3e41398a0a2cb3f9ac
tierryray <tierry.ray@gmail.com>
2019-04-11 14:41:54 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,9 +6,6 @@ const cors = require('cors');
 
 const app = express();
 
-//  Todos podem consumir e usar a aplicação
-app.use(cors());
-
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
@@ -32,6 +29,10 @@ app.use((req, res, next) => {
     //  Processa Middleware e passar adiante
     return next();
 })
+
+
+//  Todos podem consumir e usar a aplicação
+app.use(cors());
     
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
